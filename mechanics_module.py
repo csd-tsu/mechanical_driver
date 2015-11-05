284e6419510b38224a1a9acb0c186385e035bf17
olga klimenko <olga@localhost.localdomain>
2015-11-06 00:18:17 +0600

    add main function; standard scene

--- mechanics_module.py
+++ mechanics_module.py
@@ -5,67 +5,68 @@ Created on Mon Jun  1 20:29:59 2015
 @author: olga
 """
 import numpy as np
-from scipy.optimize import fsolve
 from scipy.integrate import odeint
-from scipy import argmin
-sqrt=np.sqrt
-g=9.81
+sqrt = np.sqrt
+g = 9.81
 
-def trajectory(initial_vect,x,vx,y,vy,t):
+
+def trajectory(initial_vect, x, vx, y, vy, t):
     """
-    Calculates position and velocity of objects at every 
+    Calculate positions and velocities of objects at every 
     predetermined moment of time 
     by solving the set of movement equations 
-    and adds them to the end of the lists.
+    and add them to the end of the lists.
     """
-    initial_vect=np.array(initial_vect)
-    x=list(x)
-    vx=list(vx)
-    y=list(y)
-    vy=list(vy)
+    initial_vect = np.array(initial_vect)
+    x = list(x)
+    vx = list(vx)
+    y = list(y)
+    vy = list(vy)
     
     def movement_equations(vect, t):
-       q, vq, p, vp = vect
-       f0=vq
-       f1=0
-       f2=vp
-       f3=-g
-       return [f0, f1, f2, f3]
+        q, vq, p, vp = vect
+        f0 = vq
+        f1 = 0
+        f2 = vp
+        f3 = -g
+        return [f0, f1, f2, f3]
 
-    solution=odeint(movement_equations, initial_vect, t)
-    return x+list(solution.T[0]),vx+list(solution.T[1]),y+list(solution.T[2]),vy+list(solution.T[3])
-       
-        
-    
-def search_collision(x1,x2,y1,y2,sum_of_radiuses):
+    solution = odeint(movement_equations, initial_vect, t)
+    return (x+list(solution.T[0]), vx+list(solution.T[1]),
+            y+list(solution.T[2]), vy+list(solution.T[3]))
+            
+            
+def search_collision(x1, x2, y1, y2, sum_of_radiuses):
     """
-    Calculates distances between 2 circles, 
-    compares them at all moments of time with the sum of radiuses 
-    and returns the moment of time in which 
+    Calculate distances between 2 circles, 
+    compare them at all moments of time with the sum of radius,
+    return the moment of time in which 
     the first of possible collisions occurs 
-    or 0 in the case of collision wasn't found
+    or 0 in the case of collision wasn't found.
     """
-    distance=map(lambda q1,p1,q2,p2: sqrt((q1-q2)**2+(p1-p2)**2), x1,y1,x2,y2)
-    for moment in range (len(distance)):
-       if distance[moment]<=sum_of_radiuses:
-          break
-    if moment<len(list(x1)):
-       return moment
+    distance = map(lambda q1,p1,q2,p2: sqrt((q1-q2)**2+(p1-p2)**2), x1,y1,x2,y2)
+    for moment in range(len(distance)):
+        if distance[moment] <= sum_of_radiuses:
+            break
+    if moment < len(list(x1)):
+        return moment
     else:
-       return 0
+        return 0
+
 
 def type_of_collision(x1,y1,x2,y2):
-    """Defines whether the collision is central or not"""
+    """Define whether the collision is central or not."""
     eps = 0.001
     central = 0
     if (abs(x2-x1) <= eps) or (abs(y2-y1) <= eps):
-       central = 1
+        central = 1
     return central
 
-def collision(vx1,vy1,vx2,vy2,x1,y1,x2,y2,m1,m2,r1,r2,dt,elasticity):
-    """Calculates velocities of two circles after 
-    elastic or inelastic central collision"""
-    	
+
+def collision(vx1, vy1, vx2, vy2, x1, y1, x2, y2, m1, m2, r1, r2, dt, elasticity):
+    """Calculate velocities of two circles after 
+    elastic or inelastic central collision."""
+    
     """find the normal vector (n)"""
     nx = x2-x1
     ny = y2-y1
@@ -73,7 +74,7 @@ def collision(vx1,vy1,vx2,vy2,x1,y1,x2,y2,m1,m2,r1,r2,dt,elasticity):
     """find the unit vector of normal vector (un)"""
     unx = nx/sqrt(nx**2 + ny**2)
     uny = ny/sqrt(nx**2 + ny**2)
-	
+    
     """find the unit tangent vector ut"""
     utx = -uny
     uty = unx
@@ -88,25 +89,25 @@ def collision(vx1,vy1,vx2,vy2,x1,y1,x2,y2,m1,m2,r1,r2,dt,elasticity):
     vy1t = uty * vy1
     vx2t = utx * vx2
     vy2t = uty * vy2
-	
+    
     """tangential velocities do not change after collision"""
     vx1t_new = vx1t
     vy1t_new = vy1t
     vx2t_new = vx2t
     vy2t_new = vy2t
-	
+    
     """new normal velocities"""
     vx1n_new = (m1*vx1n+m2*vx2n-(vx1n-vx2n)*m2*elasticity)/(m1+m2)
     vy1n_new = (m1*vy1n+m2*vy2n-(vy1n-vy2n)*m2*elasticity)/(m1+m2)
     vx2n_new = (m1*vx1n+m2*vx2n+(vx1n-vx2n)*m1*elasticity)/(m1+m2)
     vy2n_new = (m1*vy1n+m2*vy2n+(vy1n-vy2n)*m1*elasticity)/(m1+m2)
-	
+    
     """final velocity vectors (v_new)"""
     vx1_new = vx1n_new*unx + vx1t_new*utx
     vy1_new = vy1n_new*uny + vy1t_new*uty
     vx2_new = vx2n_new*unx + vx2t_new*utx
     vy2_new = vy2n_new*uny + vy2t_new*uty    
-   
+    
     """angular velocities after collision"""
     v1t_new = sqrt(vx1t_new**2 + vy1t_new**2)
     v2t_new = sqrt(vx2t_new**2 + vy2t_new**2)
